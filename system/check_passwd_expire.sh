db7af4b400a7dd0f1b7cc2af5a10a2f2e0f46485
361way <itybku@163.com>
2020-03-27 18:15:02 +0800

    Update check_passwd_expire.sh
    检查密码有效期，如果10天内即将过期的的帐号则进行输出。

--- system/check_passwd_expire.sh
+++ system/check_passwd_expire.sh
@@ -1,27 +1,24 @@
-#!/bin/bash
+#!/bin/bash                                                                                                                                                                      
 # ================================================================
 #   Copyright (C) 2018 www.361way.com site All rights reserved.
 #   
-#   Filename      ：check_passwd_expire.sh
+#   Filename      ：a.sh
 #   Author        ：yangbk <itybku@139.com>
 #   Create Time   ：2020-03-27 16:09
 #   Description   ：
 #   
 #   user 用户名  
-#   day 密码有效时长（自1970-01-01）
+#   day 上次修改密码的时间（自1970-01-01）
 #   mexpiry 最大时长，一般是90天或180天 ，该值为空和99999一样，永不过期 
 # ================================================================
 
 cat /etc/shadow|grep -v '!'|grep -v ':99999:'|awk -F: '{print $1,$3,$5}'|while read user day mexpiry
-do 
-    #echo $mexpiry
-
+do
     if [[ -n $mexpiry ]]; then
-        warnday=$((`date +%s`/86400 - $day - $mexpiry + 1))
-        #echo $warnday
-          if [[ $warnday -le 10 ]]; then
-                #echo $user $day $mexpiry
-                echo "`hostname`:$user will be expires in $warnday day"
-          fi
+      	warnday=$(($day + $mexpiry -`date +%s`/86400 ))
+	  if [[ $warnday -le 10 ]]; then
+	        #echo $user $day $mexpiry
+	        echo "`hostname`:$user will be expires in $warnday day"
+	  fi
     fi
 done
